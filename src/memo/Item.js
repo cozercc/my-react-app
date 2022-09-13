8435c1cab8d2f43a6ad1ee7963d41df9781c2868
cozer <cozercn@gmail.com>
2022-09-13 18:55:53 +0900

    簡易メモ課題

---
+++ src/memo/Item.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+function Item (props){
+    const th = {
+        width: '100px'
+    };
+    const td = {
+        textAlign: "right",
+        width:"150px"
+    }
+    let d = new Date(Date.parse(props.value.created));
+    let f = new
+    
+}
+
+
+export default Item
